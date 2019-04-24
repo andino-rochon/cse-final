12e50145eb87f7980b8eb587dffb485065738712
Andino Rochon <adr02288@uga.edu>
2019-04-24 10:12:28 -0400

    Updated files

--- src/project/classes/PillBottleDriver.java
+++ src/project/classes/PillBottleDriver.java
@@ -11,9 +11,6 @@ public class PillBottleDriver {
             Application.launch(PillBottle.class, args);
         } catch (UnsupportedOperationException e) {
             System.out.println(e);
-            System.err.println("If this is a DISPLAY problem, then your X server connection");
-            System.err.println("has likely timed out. This can generally be fixed by logging");
-            System.err.println("out and logging back in.");
             System.exit(1);
         } // try                                                                               
     } // main                                                                        
